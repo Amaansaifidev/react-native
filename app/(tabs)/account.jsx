1c6c4fc0e69d752ebb6599e99fe4a588118605e3
amaansaifidev <amaansaofidev@gmail.com>
2025-04-23 08:46:18 +0000

    account info

--- app/(tabs)/account.jsx
+++ app/(tabs)/account.jsx
@@ -1,18 +1,16 @@
 import { Link } from "expo-router";
 import React from "react";
-import {  View, Text, Button } from "react-native";
+import { View, Text, Pressable } from "react-native";
 
 export default function Account() {
-
-    return (
-        <View>
-            <Text>
-            this is the account page
-            </Text>
-            <Link href="/accountinfo"> 
-            <Button title="Go to Account Info" />
-            </Link>
-               
-        </View>
-    );
+  return (
+    <View>
+      <Text>this is the account page</Text>
+      <Link href="/accountinfo" asChild>
+        <Pressable>
+          <Text style={{ color: 'blue' }}>Go to Account Info</Text>
+        </Pressable>
+      </Link>
+    </View>
+  );
 }
